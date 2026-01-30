94864c630647d6d4d62b4cf40fb99a2dabdfc42c
Student Marian Rogojina (TL_KCOMP_B_Y2) <t00255407@mymtu.ie>
2026-01-30 10:37:21 +0000

    Signed-off-by: Student Marian Rogojina (TL_KCOMP_B_Y2) <t00255407@mymtu.ie>

--- Practical2026/Assets/BaseControlScript.cs
+++ Practical2026/Assets/BaseControlScript.cs
@@ -1,19 +1,36 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class BaseControlScript : MonoBehaviour
 {
+    Rigidbody rb;
+
+    private float groundDrag = 1;
+
+    private float jumpForce = 5;
+    private float jumpCooldown = 1;
+    bool readyToJump;
+
 
     float speed = 5f;
     float turnSpeed = 45f;
     Vector3 turretTarget;
 
+    private float playerHeight = 4;
+    public LayerMask whatIsGround;
+    bool grounded;
+
     Transform turret, leftGun, rightGun, gunMounting;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+
+        readyToJump = true;
+
         turretTarget = Camera.main.transform.position + Camera.main.transform.forward * 1000f;
-        turret = transform.GetChild(0);
+        turret = transform.GetChild(1);
         gunMounting = turret.GetChild(0);
         leftGun = gunMounting.GetChild(0);
         rightGun = gunMounting.GetChild(1);
@@ -27,6 +44,7 @@ public class BaseControlScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += speed * transform.forward * Time.deltaTime;
@@ -45,7 +63,35 @@ public class BaseControlScript : MonoBehaviour
             transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
         }
 
+        //JUMP 
+
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.8f + 0.3f, whatIsGround);
+
+        //-- Checks for RayCast and also prints if the tank is grounded.
+        //Debug.DrawRay(transform.position, (playerHeight * 0.5f + 0.3f) * Vector3.down);
+        //print(grounded);
+
+        //Jump keybind, checks if its grounded and the cooldown
+        if (Input.GetKey(KeyCode.Space) && readyToJump && grounded)
+        {
+
+            readyToJump = true;
+
+            Jump();
+
+            Invoke(nameof(resetJump), jumpCooldown);
+
+        }
+
         
+
+        // Handling drag 
+        if (grounded)
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
+
+
         turretTarget = RotatePointAroundPivot(turretTarget, turret.position, Quaternion.AngleAxis(Input.GetAxis("THorizontal") * turnSpeed * Time.deltaTime, transform.up));
 
         
@@ -55,14 +101,21 @@ public class BaseControlScript : MonoBehaviour
         gunMounting.LookAt(turretTarget);
 
 
+    }
 
+    //Method called after the jump is activated, with a delay by the JumpCooldown.
+    private void resetJump()
+    {
+        readyToJump = true;
+    }
 
+    //Jump boost formula
+    private void Jump()
+    {
+        // reset y velocity
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
 
-
-
-
-
-
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 
     public Vector3 RotatePointAroundPivot(Vector3 point, Vector3 pivot, Quaternion rotation)
