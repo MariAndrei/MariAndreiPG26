c7160ae0af2902afa9ad5669b768040928c9d70d
Student Marian Rogojina (TL_KCOMP_B_Y2) <t00255407@mymtu.ie>
2026-03-13 11:00:39 +0000

    Signed-off-by: Student Marian Rogojina (TL_KCOMP_B_Y2) <t00255407@mymtu.ie>

--- Practical2026/Assets/BaseControlScript.cs
+++ Practical2026/Assets/BaseControlScript.cs
@@ -1,15 +1,26 @@
 using System.Runtime.CompilerServices;
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.InputSystem.HID;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 
 public class BaseControlScript : MonoBehaviour
 {
+
+    private float range = 100f;
+    private float rayDistance = 3.5f;
+    private float damage = 25f;
+
+    float cooldown = 1.5f;
+    float leftCooldown = 0f;
+    float rightCooldown = 0f;
+
     Rigidbody rb;
 
     private float groundDrag = 1;
 
-    private float jumpForce = 5;
-    private float jumpCooldown = 1;
+    private float jumpForce = 10;
+    private float jumpCooldown = 2;
     bool readyToJump;
 
     GameObject crosshairs;
@@ -24,6 +35,11 @@ public class BaseControlScript : MonoBehaviour
     AnimationClip[] clips;
     Animator leftGunAnim, rightGunAnim;
 
+    float resetAnimation = 0.1f;
+
+    public Image IMG1;
+    public Image IMG2;
+
     Transform turret, leftGun, rightGun, gunMounting;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -37,12 +53,12 @@ public class BaseControlScript : MonoBehaviour
         leftGunAnim = animators[0];
         rightGunAnim = animators[1];
 
-        crosshairs = GameObject.Find("Crosshair");
-        if (crosshairs != null) { print("found"); }
-        UnityEngine.UI.Image ch = crosshairs.GetComponent<UnityEngine.UI.Image>();
+        //crosshairs = GameObject.Find("Crosshair");
+        //if (crosshairs != null) { print("found"); }
+        //UnityEngine.UI.Image ch = crosshairs.GetComponent<UnityEngine.UI.Image>();
 
 
-        ch.color = new Color(0, 0, 1, 0.7f);
+        //ch.color = new Color(0, 0, 1, 0.7f);
         turretTarget = Camera.main.transform.position + Camera.main.transform.forward * 1000f;
         turret = transform.GetChild(1);
         gunMounting = turret.GetChild(2);
@@ -60,6 +76,10 @@ public class BaseControlScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RaycastHit hit;
+
+        leftCooldown -= Time.deltaTime;
+        rightCooldown -= Time.deltaTime;
         
         if (Input.GetKey(KeyCode.W))
         {
@@ -79,26 +99,7 @@ public class BaseControlScript : MonoBehaviour
             transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
         }
 
-        //SHOOTING  
-
-
-        leftGunAnim.SetBool("Fire2", Input.GetMouseButtonDown(0));
-        
-
-
-        rightGunAnim.SetBool("Fire", Input.GetMouseButtonDown(1));
-        
-
-        //JUMP 
-
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.8f + 0.5f, whatIsGround);
-
-        //-- Checks for RayCast and also prints if the tank is grounded.
-        //Debug.DrawRay(transform.position, (playerHeight * 0.5f + 0.3f) * Vector3.down);
-        //print(grounded);
-
-        //Jump keybind, checks if its grounded and the cooldown
-        if (Input.GetKey(KeyCode.Space) && readyToJump && grounded)
+        if (Input.GetKeyDown(KeyCode.Space) && readyToJump && grounded)
         {
 
             readyToJump = true;
@@ -109,7 +110,7 @@ public class BaseControlScript : MonoBehaviour
 
         }
 
-        
+
 
         // Handling drag 
         if (grounded)
@@ -117,6 +118,65 @@ public class BaseControlScript : MonoBehaviour
         else
             rb.linearDamping = 0;
 
+
+        //SHOOTING  
+
+
+
+        if (Input.GetMouseButtonDown(0) && leftCooldown <= 0)
+        {
+            leftGunAnim.SetBool("Fire2", true);
+            leftCooldown = cooldown;
+            if (Physics.Raycast(Camera.main.transform.position + transform.forward * rayDistance, Camera.main.transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+
+
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+
+                    target.takeDamage(damage);
+
+                }
+            }
+        }
+        if ((cooldown - leftCooldown) > resetAnimation) { leftGunAnim.SetBool("Fire2", false); }
+
+        IMG1.fillAmount = Mathf.Clamp((cooldown - leftCooldown)/cooldown, 0, 1);
+
+        
+        if (Input.GetMouseButtonDown(1) && rightCooldown <= 0)
+        {
+            rightGunAnim.SetBool("Fire", true);
+            rightCooldown = cooldown;
+            if (Physics.Raycast(Camera.main.transform.position + transform.forward * rayDistance, Camera.main.transform.forward, out hit, range))
+            {
+                Debug.Log(hit.transform.name);
+
+
+                Target target = hit.transform.GetComponent<Target>();
+                if (target != null)
+                {
+
+                    target.takeDamage(damage);
+
+                }
+            }
+        }
+        if ((cooldown - rightCooldown) > resetAnimation) { rightGunAnim.SetBool("Fire", false); }
+
+        IMG2.fillAmount = Mathf.Clamp((cooldown - rightCooldown) / cooldown, 0, 1);
+        //JUMP 
+
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 1f + 0.1f, whatIsGround);
+
+        //-- Checks for RayCast and also prints if the tank is grounded.
+        //Debug.DrawRay(transform.position, (playerHeight * 0.5f + 0.3f) * Vector3.down);
+        //print(grounded);
+
+        //Jump keybind, checks if its grounded and the cooldown
+        
 
         turretTarget = RotatePointAroundPivot(turretTarget, turret.position, Quaternion.AngleAxis(Input.GetAxis("THorizontal") * turnSpeed * Time.deltaTime, transform.up));
 
--- Practical2026/Assets/TurretControlScript.cs
+++ Practical2026/Assets/TurretControlScript.cs
@@ -1,52 +1,20 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class TurretControlScript : MonoBehaviour
 {
-    //private float damage = 50f; 
-    private float range = 100f;
-    private float rayDistance = 3.5f;
-    private float damage = 25f;
+    
+    
 
+    
 
     void Update()
     {
 
-        RaycastHit hit;
+      
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (Physics.Raycast(Camera.main.transform.position + transform.forward * rayDistance, Camera.main.transform.forward, out hit, range)) 
-            {
-                Debug.Log(hit.transform.name);
-
-                Target target = hit.transform.GetComponent<Target>();
-                if (target != null)
-                {
-
-                    target.takeDamage(damage);
-
-                }
-            }
-
-        }
-
-        if (Input.GetMouseButtonDown(1))
-        {
-
-            if (Physics.Raycast(Camera.main.transform.position+transform.forward * rayDistance, Camera.main.transform.forward, out hit, range))
-            {
-                Debug.Log(hit.transform.name);
-
-                Target target = hit.transform.GetComponent<Target>();
-                if (target != null) {
-
-                    target.takeDamage(damage);
-                    
-                }
-
-            }
-
-        }
+        
     }
 }
